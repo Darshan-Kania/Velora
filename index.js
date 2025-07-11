0fc4659a4a2071b06d72de04b1f2072fe1f60272
Darshan Kania <darshankania2604@gmail.com>
2025-07-11 18:15:26 +0530

    Fix server listener to bind to all network interfaces

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use("/auth",authRoute)
 // Webhook
 app.use("/webhook", require("./routes/webhook"));
 // Listener
-app.listen(process.env.PORT,() => {
+app.listen(process.env.PORT,'0.0.0.0',() => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
