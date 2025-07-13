1677b2744497cbe97ecf2732023f60b1d45bd1eb
Darshan Kania <darshankania2604@gmail.com>
2025-07-13 12:35:40 +0530

    Add logger import to webhook and Google Auth services

--- service/googleAuth.js
+++ service/googleAuth.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const watchGmailInbox=require("./watchGmail");
+const logger = require("../utils/logger");
 passport.use(
   new GoogleStrategy(
     {
