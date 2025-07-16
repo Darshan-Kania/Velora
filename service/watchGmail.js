4e1bb69ead12061b4d87f6a8e689f5c7cd51c1e0
Darshan Kania <darshankania2604@gmail.com>
2025-07-16 19:21:17 +0530

    Refactor watchGmailInbox function for improved error handling and logging

--- service/watchGmail.js
+++ service/watchGmail.js
@@ -1,26 +1,21 @@
 const { google } = require("googleapis");
 const logger = require("../utils/logger");
 async function watchGmailInbox(accessToken) {
-  try {
-    const auth = new google.auth.OAuth2();
-    auth.setCredentials({ access_token: accessToken });
+  const auth = new google.auth.OAuth2();
+  auth.setCredentials({ access_token: accessToken });
 
-    const gmail = google.gmail({ version: "v1", auth });
+  const gmail = google.gmail({ version: "v1", auth });
 
-    const res = await gmail.users.watch({
-      userId: "me",
-      requestBody: {
-        topicName: "projects/mailflare-465412/topics/mailflareGmail",
-        labelIds: ["INBOX"],
-        labelFilterAction: "include",
-      },
-    });
+  const res = await gmail.users.watch({
+    userId: "me",
+    requestBody: {
+      topicName: "projects/mailflare-465412/topics/mailflareGmail",
+      labelIds: ["INBOX"],
+      labelFilterAction: "include",
+    },
+  });
 
-    logger.info("✅ Gmail watch started:", res.data);
-    return res.data;
-  } catch (error) {
-    logger.error("❌ Error starting Gmail watch:", error);
-    throw new Error("Failed to start Gmail watch");
-  }
-}
+  logger.info("✅ Gmail watch started:", res.data);
+  return res.data;
+} 
 module.exports = watchGmailInbox;
