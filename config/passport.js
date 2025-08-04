cdcd004513fd33943a30af92b62577bf36cdc7a8
Darshan Kania <darshankania2604@gmail.com>
2025-08-04 17:58:34 +0530

    refactor: remove commented note about loading environment variables from .env

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,6 @@ const passport = require("passport");
 const logger = require("../utils/logger");
 const dotenv = require("dotenv").config();
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-// You can also load these from .env
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "YOUR_CLIENT_ID";
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || "YOUR_CLIENT_SECRET";
 
@@ -12,14 +11,6 @@ passport.use(
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      // You can handle database logic here if needed
-      return done(null, profile);
-    }
-  )
-);
-
     },
     (accessToken, refreshToken, profile, done) => {
       // You can handle database logic here if needed
